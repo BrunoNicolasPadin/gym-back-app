47116fe0d7f62f8f2c61b03f047b746240b57cef
brunonp_ <brunonicolaspadin@gmail.com>
2023-07-21 00:00:39 -0300

    Add lov

--- app/Http/Controllers/LovController.php
+++ app/Http/Controllers/LovController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\LovStoreRequest;
 use App\Models\Lov;
 use Illuminate\Http\Request;
 use Spatie\QueryBuilder\QueryBuilder;
+use Symfony\Component\HttpFoundation\Response;
 
 class LovController extends Controller
 {
@@ -12,14 +14,16 @@ class LovController extends Controller
     {
         return QueryBuilder::for(Lov::class)
         ->allowedFilters(['label', 'category'])
-        ->paginate(1)
+        ->paginate(10)
         ->appends(request()->query());
     
     }
 
-    public function store(Request $request)
+    public function store(LovStoreRequest $request)
     {
-        //
+        Lov::create($request->validated());
+
+        return response('', Response::HTTP_CREATED);
     }
 
     public function show(string $id)
